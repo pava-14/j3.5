afb4ed8820bbe0295e4f5d28a365ecdaf79d1cb1
Alexander Pavlenko <pava@bk.ru>
2020-06-15 16:00:54 +0700

    Classes added

---
+++ src/test/java/ru/netology/ru/netoogy/manager/ProductManagerTest.java
@@ -0,0 +1,13 @@
+package ru.netology.ru.netoogy.manager;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductManagerTest {
+
+    @Test
+    void shouldSearchBy() {
+        //
+    }
+}
